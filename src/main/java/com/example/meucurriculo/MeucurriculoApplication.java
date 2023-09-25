af9b10554abfb68eab1cc919eccfd584f9cb3e6f
juniorzs2 <josebezerra.jr3@gmail.com>
2023-09-25 16:59:44 -0300

    Meucurriculo

---
+++ src/main/java/com/example/meucurriculo/MeucurriculoApplication.java
@@ -0,0 +1,13 @@
+package com.example.meucurriculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucurriculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeucurriculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meucurriculo/MeucurriculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meucurriculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeucurriculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
